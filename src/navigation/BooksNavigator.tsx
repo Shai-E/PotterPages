4bf60c9b59663398571c1cc2b035a2a4c7d3aa34
Shai Eliav <yuvaleliav@Shais-MBP.lan>
2024-08-01 13:07:39 +0300

    basic design and re-title headers

--- src/navigation/BooksNavigator.tsx
+++ src/navigation/BooksNavigator.tsx
@@ -3,17 +3,32 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BooksScreen from '../screens/BooksScreen';
 import BookDetailScreen from '../screens/BookDetailScreen';
 import {BooksStackParamList} from './types';
+import {light} from '../fixtures/colors.json';
 
 const Stack = createNativeStackNavigator<BooksStackParamList>();
 
 const BooksNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="BooksScreen" component={BooksScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerBackTitle: 'Back',
+        // headerBackTitleStyle: {fontSize: 16},
+        headerStyle: {
+          backgroundColor: light.primary,
+        },
+        headerTintColor: 'white',
+      }}>
+      <Stack.Screen
+        name="BooksScreen"
+        component={BooksScreen}
+        options={{title: 'Potter Pages'}}
+      />
+
       <Stack.Screen
         name="BookDetail"
         component={BookDetailScreen}
-        options={{headerShown: true}}
+        options={{headerShown: true, title: 'Book Details'}}
       />
     </Stack.Navigator>
   );
--- src/navigation/FavoritesNavigator.tsx
+++ src/navigation/FavoritesNavigator.tsx
@@ -3,17 +3,31 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import BookDetailScreen from '../screens/BookDetailScreen';
 import {FavoritesStackParamList} from './types';
 import FavoritesScreen from '../screens/FavoritesScreen';
+import {light} from '../fixtures/colors.json';
 
 const Stack = createNativeStackNavigator<FavoritesStackParamList>();
 
 const FavoritesNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="FavoritesScreen" component={FavoritesScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerBackTitle: 'Back',
+        // headerBackTitleStyle: {fontSize: 16},
+        headerStyle: {
+          backgroundColor: light.primary,
+        },
+        headerTintColor: 'white',
+      }}>
+      <Stack.Screen
+        name="FavoritesScreen"
+        component={FavoritesScreen}
+        options={{title: 'Favorites'}}
+      />
       <Stack.Screen
         name="BookDetail"
         component={BookDetailScreen}
-        options={{headerShown: true}}
+        options={{headerShown: true, title: 'Book Details'}}
       />
     </Stack.Navigator>
   );
