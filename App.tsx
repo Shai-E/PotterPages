6f402e4f98db539060ab0437ef1e56660a32e457
Shai Eliav <yuvaleliav@Shais-MBP.lan>
2024-08-02 00:41:35 +0300

    navigation bar android color

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
+import {Platform} from 'react-native';
 import {Provider} from 'react-redux';
 import {persistor, store} from './src/store/store';
 import {PersistGate} from 'redux-persist/integration/react';
 import AppNavigation from './src/navigation/AppNavigation';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
+import {light} from './src/fixtures/colors.json';
 
 const App = () => {
+  useLayoutEffect(() => {
+    try {
+      Platform.OS === 'android' && changeNavigationBarColor(light.primary);
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
